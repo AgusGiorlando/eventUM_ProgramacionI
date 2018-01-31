968f23fc0a65c7a1c3523f0d4c5c9bcefab0ba78
AgusGiorlando <agus.giorlando@gmail.com>
2018-01-31 13:54:09 -0300

    agregado recuperarClave.php

--- enviarEmail.php
+++ enviarEmail.php
@@ -1,15 +1,47 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    //VERIFICACION DE USUARIO
+    //MySQL
+    $servidor = "localhost";
+    $bd = "eventum";
+    
+    $conexion = new PDO("mysql:host=$servidor;port=3306;dbname=$bd;charset=utf8","root","");
+    
+    $sql = "SELECT * FROM usuarios WHERE email = pepe@e.com ;";
+    $ejecucion = $conexion->prepare($sql);
+    $ejecucion->execute();
+    
+    if(){
+
+    }else{
+
+    }
+
+    //EMAIL
+    //Parametros
     $mensaje = "Linea 1\r\nLinea 2\r\nLinea3\r\n";
     $asunto = "Prueba de recuperacion de clave";
     $destino = "a.giorlando@alumno.um.edu.ar";
+    
     //Cabeceras
     $cabecera = "MIME-Version: 1.0\r\n";
     $cabecera .= "Content-type: text/html; charset=iso-8859-1\r\n";
     $cabecera .= "From: agus.giorlando@gmail.com\r\n";
 
+    //Envio
     if(mail($destino,$asunto,$mensaje,$cabecera)){
-        echo "Mensaje enviado";
+        echo "<h1>Mensaje enviado</h1>";
     }else{
-        echo "Mensaje no enviado";
+        echo "<h1>Mensaje no enviado</h1>";
     }
-?>
+    ?>    
+</body>
+</html>
