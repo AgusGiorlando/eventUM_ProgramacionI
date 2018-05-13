58ad03e0f685eb069f001e535aac9a85e25ea9a4
AgusGiorlando <agus.giorlando@gmail.com>
2018-05-13 17:55:46 -0300

    no message

---
+++ mostrarCarpeta.php
@@ -0,0 +1,36 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>PRUEBA</title>
+    </head>
+    <body>
+        <pre>
+        <?php
+        /**
+ * Funcion que muestra la estructura de carpetas a partir de la ruta dada.
+ */
+mostrarCarpeta(getcwd()."\docs");
+function mostrarCarpeta($ruta){
+    // Se comprueba que realmente sea la ruta de un directorio
+    if (is_dir($ruta)){
+        // Abre un gestor de directorios para la ruta indicada
+        $gestor = opendir($ruta);
+        echo "<ul>";
+        // Recorre todos los elementos del directorio
+        while (($archivo = readdir($gestor)) !== false)  {
+            $ruta_completa = $ruta . "/" . $archivo;
+            // Se muestran todos los archivos y carpetas excepto "." y ".."
+            if ($archivo != "." && $archivo != "..") {
+                // Si es un directorio se recorre recursivamente
+                    echo "<li>" . $archivo . "</li>";
+                }
+            }
+        }       
+        // Cierra el gestor de directorios
+        closedir($gestor);
+        echo "</ul>";
+}
+        ?>
+        </pre>
+    </body>
+</html>
